4ab83f8bd6e6c63fd565d06e5e713d684cf36c53
dbc2201 <divyanshb22@gmail.com>
2019-04-18 15:31:25 +0530

    add method:display to show the content of the array at any time

--- src/main/BubbleSort.java
+++ src/main/BubbleSort.java
@@ -11,7 +11,21 @@ public class BubbleSort {
         numElements = 0;    //  there are no items in the array yet
     }
 
+    /*
+     * method to add values to the array
+     * */
     public void insert(long value) {
         array[numElements] = value;
+        numElements++;
+        /*
+         * this way, we can store the current number of elements in the array
+         * */
+    }
+
+    /*
+     * method to display the content of the array
+     * */
+    public void display() {
+
     }
 }
