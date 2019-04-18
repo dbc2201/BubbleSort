f4a03503592e2193c31921447038bc9bfffafaac
dbc2201 <divyanshb22@gmail.com>
2019-04-18 15:27:33 +0530

    create a constructor for class:BubbleSort

--- src/main/BubbleSort.java
+++ src/main/BubbleSort.java
@@ -1,4 +1,12 @@
 package main;
 
 public class BubbleSort {
+
+    private long[] array;   //  this array will hold the data items for sorting
+
+    private int numElements;    //  this is the number of elements the array can have
+
+    public BubbleSort(int n) {
+        array = new long[n];    //  create the array with 'n' items
+    }
 }
